9d2e7df9ec893338a133221c81684f25f56f78f2
Weiss Bai <whitesgr03@gmail.com>
2024-08-27 16:04:56 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,59 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import express from "express";
+import createError from "http-errors";
+import morgan from "morgan";
+import debug from "debug";
+import compression from "compression";
+import helmet from "helmet";
+
+// routes
+import indexRouter from "./routes/index.js";
+import fileUploadRouter from "./routes/fileUpload.js";
+
+const app = express();
+const errorLog = debug("ServerError");
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const helmetOptions = {
+	contentSecurityPolicy: {
+		directives: {
+			styleSrc: ["'self'", "fonts.googleapis.com", "necolas.github.io"],
+		},
+	},
+};
+const staticOptions = {
+	index: false,
+	redirect: false,
+};
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(helmet(helmetOptions));
+app.use(express.static(path.join(__dirname, "public"), staticOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan(process.env.production ? "common" : "dev"));
+app.use(compression());
+
+app.get("/favicon.ico", (req, res) => res.status(204));
+app.use("/", indexRouter);
+app.use("/drive", fileUploadRouter);
+
+// unknown routes handler
+app.use((req, res, next) => {
+	next(createError(404, "The endpoint you are looking for cannot be found."));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	errorLog(err);
+
+	err.status ?? (err = createError(500));
+
+	res.render("error", { message: err.message });
+});
+
+export default app;
---
+++ index.js
@@ -0,0 +1,40 @@
+import os from "node:os";
+import debug from "debug";
+
+import app from "./app.js";
+
+const serverLog = debug("Server");
+
+const port = process.env.PORT || "3000";
+
+const handleServer = async () => {
+	const handleListening = async () => {
+		const IP_Address = os
+			.networkInterfaces()
+			.en0.find(internet => internet.family === "IPv4").address;
+		serverLog(`Listening on Local:        http://localhost:${port}`);
+		serverLog(`Listening on Your Network: http://${IP_Address}:${port}`);
+	};
+	const handleError = error => {
+		switch (error.code) {
+			case "EACCES":
+				serverLog(`Port ${port} requires elevated privileges`);
+				app.close();
+			case "EADDRINUSE":
+				serverLog(`Port ${port} is already in use`);
+				app.close();
+			default:
+				serverLog(error);
+		}
+	};
+	const handleClose = () => {
+		serverLog(`There has an Error, so the server is closed.`);
+		process.exit(1);
+	};
+
+	app.listen(port, process.env.NODE_ENV === "development" && handleListening)
+		.on("error", handleError)
+		.on("close", handleClose);
+};
+
+handleServer();
