4aca3e7abbd146c07dc850c233a8713ec22be7a8
Weiss Bai <whitesgr03@gmail.com>
2024-08-30 08:39:36 +0800

    Add handle HTTPS

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-import os from "node:os";
+import { networkInterfaces } from "node:os";
 import debug from "debug";
+import { createServer } from "node:https";
+import { readFileSync } from "node:fs";
 
 import app from "./app.js";
 
 const serverLog = debug("Server");
 
-const port = process.env.PORT || "3240";
+const port = process.env.PORT || "3000";
+const develop = process.env.NODE_ENV === "development";
 
 const handleServer = async () => {
 	const handleListening = async () => {
-		const IP_Address = os
-			.networkInterfaces()
-			.en0.find(internet => internet.family === "IPv4").address;
-		serverLog(`Listening on Local:        http://localhost:${port}`);
-		serverLog(`Listening on Your Network: http://${IP_Address}:${port}`);
+		const IP_Address = networkInterfaces().en0.find(
+			internet => internet.family === "IPv4",
+		).address;
+
+		const scheme = `http${develop ? "s" : ""}`;
+
+		serverLog(`Listening on Local:         ${scheme}://localhost:${port}`);
+		serverLog(
+			`Listening on Your Network:  ${scheme}://${IP_Address}:${port}`,
+		);
 	};
 	const handleError = error => {
 		switch (error.code) {
@@ -32,7 +40,18 @@ const handleServer = async () => {
 		process.exit(1);
 	};
 
-	app.listen(port, process.env.NODE_ENV === "development" && handleListening)
+	const handleHTTPS = () => {
+		const options = {
+			key: readFileSync("key.pem"),
+			cert: readFileSync("cert.pem"),
+		};
+		return createServer(options, app);
+	};
+
+	const server = develop ? handleHTTPS() : app;
+
+	server
+		.listen(port, develop && handleListening)
 		.on("error", handleError)
 		.on("close", handleClose);
 };
