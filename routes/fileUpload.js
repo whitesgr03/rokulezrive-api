f3063d647a2f53307ec860356719665f806f711e
Weiss Bai <whitesgr03@gmail.com>
2024-08-31 16:49:36 +0800

    refactor(route): root path renders home file

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("index"));
+router.get("/", (req, res) => res.render("home"));
 
 export default router;
