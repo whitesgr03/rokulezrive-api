cfa418223b7c6b3e184c7707a58250cbff2cdf6a
Weiss Bai <whitesgr03@gmail.com>
2024-09-01 18:32:02 +0800

    feat: add a drive route to root path

--- routes/drive.js
+++ routes/drive.js
@@ -1,4 +1,5 @@
 import express from "express";
 const router = express.Router();
-router.get("/", (req, res) => res.render("file_list"));
+router.get("/", (req, res) => res.redirect("/drive/files"));
+router.get("/files", (req, res) => res.render("file_list"));
 export default router;
