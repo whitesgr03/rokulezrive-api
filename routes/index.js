8d35606b3d5151c3ca5611faae4de7e4497601be
Weiss Bai <whitesgr03@gmail.com>
2024-09-01 18:33:23 +0800

    refactor: root path redirect to files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.redirect("/drive"));
+router.get("/", (req, res) => res.redirect("/drive/files"));
 
 export default router;
