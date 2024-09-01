e6b4cd691b5abe76661909d0a2daac45e9383813
Weiss Bai <whitesgr03@gmail.com>
2024-09-01 08:46:30 +0800

    feat: click a options button to display a list of options

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,14 +6,70 @@ const handleClick = e => {
 	const handleCloseAccountList = () => {
 		dropdown.classList.remove("active");
 	};
+	const handleDeleteOptionList = () => {
+		const optionList = document.querySelector(".optionList");
+		optionList?.remove();
+	};
+
 	const handleActiveAccountList = () => {
+		handleDeleteOptionList();
 		dropdown.classList.toggle("active");
 		dropdown.classList.add("slide");
 	};
+
+	const handleCreateOptionList = e => {
+		handleDeleteOptionList();
+		handleCloseAccountList();
+		const parent = e.target.closest(".fileItem");
+		const items = [
+			{
+				class: "share",
+				text: "Share",
+			},
+			{
+				class: "download",
+				text: "Download",
+			},
+			{
+				class: "edit",
+				text: "Rename",
+			},
+			{
+				class: "delete",
+				text: "Remove",
+			},
+		];
+
+		const list = document.createElement("ul");
+		list.classList.add("optionList");
+
+		const createItem = item => {
+			const content = `
+				<button type="button">
+					<span class="icon ${item.class}"><span/>
+				</button>
+			`;
+
+			const listItem = document.createElement("li");
+			listItem.innerHTML = content;
+			listItem.querySelector("button").append(item.text);
+
+			return listItem;
+		};
+
+		items.forEach(item => list.append(createItem(item)));
+
+		parent.append(list);
+	};
+
 	const handleClose = e => {
 		!e.target.closest(".account") &&
 			!e.target.closest(".dropdown") &&
 			handleCloseAccountList();
+
+		!e.target.closest(".optionBtn") &&
+			!e.target.closest(".optionList") &&
+			handleDeleteOptionList();
 	};
 	const handleDarkTheme = () => {
 		const darkTheme = document.body.classList.toggle("dark");
@@ -21,6 +77,7 @@ const handleClick = e => {
 	};
 
 	e.target.closest(".account") && handleActiveAccountList();
+	e.target.closest(".optionBtn") && handleCreateOptionList(e);
 	e.target.closest(".theme") && handleDarkTheme();
 	handleClose(e);
 };
