394c4af5152657a27703d645aa74ac9b77e89564
Weiss Bai <whitesgr03@gmail.com>
2024-09-02 15:22:04 +0800

    feat: create register route

--- routes/account.js
+++ routes/account.js
@@ -7,4 +7,8 @@ const router = express.Router();
 router
 	.route("/login")
 	.get(userControllers.loginGet)
+router
+	.route("/register")
+	.get(userControllers.registerGet)
+	.post(userControllers.registerPost);
 export default router;
