2b1c5e5e82ee19a59d0e5894ebef08aa9af1cae6
Weiss Bai <whitesgr03@gmail.com>
2024-09-02 13:10:11 +0800

    feat: create passport authentication

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+import { Strategy as LocalStrategy } from "passport-local";
+
+const prisma = new PrismaClient();
+
+passport.use(
+	new LocalStrategy(
+		{ usernameField: "email" },
+		async (email, password, done) => {
+			try {
+				const user = await prisma.user.findFirst({ where: { email } });
+				const match =
+					user && (await bcrypt.compare(password, user.password));
+
+				match
+					? done(null, {
+							id: user.id,
+						})
+					: done(null, false, "The account could not be found.");
+			} catch (err) {
+				done(err);
+			}
+		},
+	),
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+export default passport;
