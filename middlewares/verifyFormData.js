d87c0e6c82e57a4401a1cf1de98cb3f7b6de774c
Weiss Bai <whitesgr03@gmail.com>
2024-09-02 15:28:30 +0800

    feat: create validation form data middleware supported by express-validator

---
+++ middlewares/verifyFormData.js
@@ -0,0 +1,34 @@
+import asyncHandler from "express-async-handler";
+import { validationResult, checkSchema, matchedData } from "express-validator";
+import Csrf from "csrf";
+
+const verifyFormData = ({ schema, template }) => {
+	return asyncHandler(async (req, res, next) => {
+		await checkSchema(schema, ["body"]).run(req);
+
+		const schemaErrors = validationResult(req);
+
+		const handleSchemaErrors = async () => {
+			const inputErrors = schemaErrors.mapped();
+
+			const csrf = new Csrf();
+			const secret = await csrf.secret();
+			req.session.csrf = secret;
+
+			res.render(template, {
+				data: req.body,
+				csrfToken: csrf.create(secret),
+				inputErrors,
+			});
+		};
+
+		const setMatchData = () => {
+			req.data = matchedData(req);
+			next();
+		};
+
+		schemaErrors.isEmpty() ? setMatchData() : handleSchemaErrors();
+	});
+};
+
+export default verifyFormData;
