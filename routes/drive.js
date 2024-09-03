db827d389f483aca2d17986a803c36b401fc0495
Weiss Bai <whitesgr03@gmail.com>
2024-09-03 11:26:33 +0800

    refactor: drive route root path rendering index page

--- routes/drive.js
+++ routes/drive.js
@@ -1,5 +1,5 @@
 import express from "express";
 const router = express.Router();
-router.get("/", (req, res) => res.redirect("/drive/files"));
+router.get("/", (req, res) => res.render("index"));
 router.get("/files", (req, res) => res.render("file_list"));
 export default router;
