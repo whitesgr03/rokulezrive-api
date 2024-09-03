c3360e4e19b3259824d0c533d40ded400e866a68
Weiss Bai <whitesgr03@gmail.com>
2024-09-03 22:56:43 +0800

    feat: create sharedList middleware

--- controllers/file.js
+++ controllers/file.js
@@ -2,6 +2,9 @@ import asyncHandler from "express-async-handler";
 const fileList = asyncHandler((req, res) =>
 	res.render("drive", { files: true }),
 );
+const sharedList = asyncHandler((req, res) =>
+	res.render("drive", { shared: true }),
+);
 const index = asyncHandler((req, res) =>
 	res.render("drive", {
 		files: true,
