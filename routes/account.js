be51746c2b29462c10a9570e814adb31506cabc3
Weiss Bai <whitesgr03@gmail.com>
2024-09-12 16:00:12 +0800

    style: change double quote to  single quote

--- routes/account.js
+++ routes/account.js
@@ -1,17 +1,17 @@
-import express from "express";
+import express from 'express';
 
-import * as userControllers from "../controllers/user.js";
+import * as userControllers from '../controllers/user.js';
 
 const router = express.Router();
 
 router
-	.route("/login")
+	.route('/login')
 	.get(userControllers.loginGet)
 	.post(userControllers.loginPost);
 router
-	.route("/register")
+	.route('/register')
 	.get(userControllers.registerGet)
 	.post(userControllers.registerPost);
 
-router.get("/logout", userControllers.logout);
+router.get('/logout', userControllers.logout);
 export default router;
--- routes/drive.js
+++ routes/drive.js
@@ -1,6 +1,6 @@
-import express from "express";
+import express from 'express';
 
-import * as fileControllers from "../controllers/file.js";
+import * as fileControllers from '../controllers/file.js';
 
 const router = express.Router();
 
@@ -8,11 +8,13 @@ router.use((req, res, next) => {
 	req.isAuthenticated() ? next() : res.redirect("/");
 });
 
-router.route("/files/create").get(fileControllers.fileCreateGet);
-router.get("/files", fileControllers.fileList);
+router
+  .route('/files/create')
+  .get(fileControllers.fileCreateGet);
+router.get('/files', fileControllers.fileList);
 
-router.get("/shared", fileControllers.sharedList);
+router.get('/shared', fileControllers.sharedList);
 
-router.get("/", fileControllers.index);
+router.get('/', fileControllers.index);
 
 export default router;
