28d7dcee9cf943b2114029f82ac989b8e4a89d6e
Weiss Bai <whitesgr03@gmail.com>
2024-09-19 09:00:32 +0800

    feat!: remove the HTTP Get path of the login route and register route

--- routes/account.js
+++ routes/account.js
@@ -4,14 +4,7 @@ import * as userControllers from '../controllers/user.js';
 
 const router = express.Router();
 
-router
-	.route('/login')
-	.get(userControllers.loginGet)
-	.post(userControllers.loginPost);
-router
-	.route('/register')
-	.get(userControllers.registerGet)
-	.post(userControllers.registerPost);
-
+router.post('/login', userControllers.login);
+router.post('/register', userControllers.register);
 router.get('/logout', userControllers.logout);
 export default router;
