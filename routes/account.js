adda173fb423e3d4e4fe51738fff48e6a53250df
Weiss Bai <whitesgr03@gmail.com>
2024-09-22 16:58:35 +0800

    feat: add google login and register routes

--- routes/account.js
+++ routes/account.js
@@ -8,4 +8,7 @@ router.get('/user', userControllers.userInfo)
 router.post('/login', userControllers.login);
 router.post('/register', userControllers.register);
 router.get('/logout', userControllers.logout);
+
+router.post('/login/google', userControllers.googleUserLogin);
+router.post('/register/google', userControllers.googleUserRegister);
 export default router;
