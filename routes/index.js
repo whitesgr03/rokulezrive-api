579f4004bb040806e2bbb81d0ec08f9c5b91c5ec
Weiss Bai <whitesgr03@gmail.com>
2024-09-25 09:24:33 +0800

    feat: handle logout using http get method

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/user', userControllers.getUser);
 router.post('/login', userControllers.login);
 router.post('/register', userControllers.register);
-router.post('/logout', userControllers.logout);
+router.get('/logout', userControllers.logout);
 
 router.post('/login/google', userControllers.loginWithGoogle);
 router.post('/register/google', userControllers.registerWithGoogle);
