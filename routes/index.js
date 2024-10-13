cd3a18144947d4b86be79bbdc6978472897c2037
Weiss Bai <whitesgr03@gmail.com>
2024-10-14 02:06:07 +0800

    feat: add verifyCSRF to index routes

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 
 import * as userControllers from '../controllers/user.js';
 
+import { verifyCSRF } from '../middlewares/verifyCSRF.js';
+
 const router = express.Router();
 
-router.post('/login', userControllers.login);
-router.post('/register', userControllers.register);
-router.post('/logout', userControllers.logout);
+router.post('/login', verifyCSRF, userControllers.login);
+router.post('/register', verifyCSRF, userControllers.register);
+router.post('/logout', verifyCSRF, userControllers.logout);
 
-router.post('/login/google', userControllers.loginWithGoogle);
-router.post('/register/google', userControllers.registerWithGoogle);
-router.post('/login/facebook', userControllers.loginWithFacebook);
-router.post('/register/facebook', userControllers.registerWithFacebook);
+router.post('/login/google', verifyCSRF, userControllers.loginWithGoogle);
+router.post('/register/google', verifyCSRF, userControllers.registerWithGoogle);
+router.post('/login/facebook', verifyCSRF, userControllers.loginWithFacebook);
+router.post(
+	'/register/facebook',
+	verifyCSRF,
+	userControllers.registerWithFacebook
+);
 
 export default router;
