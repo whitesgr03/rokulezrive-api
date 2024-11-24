a54642386785fc3ab46f32449f13f4fc2e404631
Weiss Bai <whitesgr03@gmail.com>
2024-11-24 23:38:13 +0800

    feat: change include and exclude patterns

--- __test__/integration/vitest.config.js
+++ __test__/integration/vitest.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['__test__/integration/*.test.js'],
-		exclude: ['__test__/integration/example_*.test.js'],
+		include: ['__test__/integration/**.test.js'],
+		exclude: ['__test__/integration/example_**.test.js'],
 		setupFiles: ['__test__/integration/setup.js'],
 		poolOptions: {
 			forks: {
